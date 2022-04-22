0db25e30a2acf0a1fdb23216f511be5186dcad94
ponideal <goldbiz@bk.ru>
2022-04-22 15:30:36 -0400

    123

--- controller_install.sh
+++ controller_install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 WORK_DIR="/home/user/autoqubes"
-ANSIBLE_CONFIG_ENV_DIR="$WORK_DIR/ansible-qubes/ansible.cfg"
-
-RPCDIR="/etc/qubes-rpc"
-NAME_BOBMSHELL_CLIENT="controller"
-BASED_VM="fedora-34"
-
-ANSIBLE_NET_VM="sys-firewall"
+HOME_DIR="/home/user"
 ANSIBLE_ROOT_DIR="/etc/ansible"
+BASH_RC_PATH="$HOME_DIR/.bashrc"
+QRUN_PATH="/home/user/ansible-qubes/bin"
 
 
-sudo git clone https://github.com/ponideal/autoqubes.git
-
+#transfer confuguration in controller
 sudo rm -rf $ANSIBLE_ROOT_DIR/
 
-sudo cp -f $ANSIBLE_CONFIG_ENV_DIR /etc/ansible/ansible.cfg
+#transef ansible cfg 
+sudo cp -rf $WORK_DIR/ansible-qubes/ansible.cfg $ANSIBLE_ROOT_DIR/ansible.cfg
 
-sudo cp -f $WORK_DIR/hosts $ANSIBLE_ROOT_DIR/hosts
+#transef host file
+sudo cp -rf $WORK_DIR/ansible-qubes/hosts $ANSIBLE_ROOT_DIR/hosts
 
-sudo ansible dom0 -m ping
+#create PATH for qrun, bombshell
+if grep -q $QRUN_PATH $BASH_RC_PATH
+	then echo "PATH $QRUN_PATH in $BASH_RC_PATH file is exist!"
+else 
+	sudo echo "export PATH=\$PATH:$QRUN_PATH" >> $BASH_RC_PATH
+	echo "PATH $QRUN_PATH in $BASH_RC_PATH DONE!"
+fi
+
+#ping test to dom0
+ansible dom0 -m ping
--- install.sh
+++ install.sh
@@ -2,43 +2,54 @@
 
 WORK_DIR="/home/user/autoqubes"
 
-ANSIBLE_CONFIG_ENV_DIR="$WORK_DIR/ansible-qubes/ansible.cfg"
 ANSIBLE_ROOT_DIR="/etc/ansible"
 ANSIBLE_NET_VM="sys-firewall"
 
 RPCDIR="/etc/qubes-rpc"
 NAME_BOBMSHELL_CLIENT="controller"
-BASED_VM="fedora-34"
-
+BASED_VM="fedora-34-clone"
 
 #update dom0
-sudo qubes-dom0-update --clean -y
+#sudo qubes-dom0-update --clean -y
 
 #create controller VM - clone BASEDVM
-qvm-clone "$BASED_VM" "$NAME_BOBMSHELL_CLIENT" -v
+#qvm-clone "$BASED_VM" "$NAME_BOBMSHELL_CLIENT-TEMPLATE" -v
 
+#qvm create 
+qvm-create --prop template=$BASED_VM --prop name=$NAME_BOBMSHELL_CLIENT --label=red
 #update BOBBSHELL client and BASED VM
 #sudo qubes-dom0-update --action=upgrade "qubes-template-$NAME_BOBMSHELL_CLIENT"
 #sudo qubes-dom0-update --action=upgrade "qubes-template-$BASED_VM"
 
 #active service in dom0
+sudo rm -rf "$RPCDIR/qubes.VMShell"
 sudo touch "$RPCDIR/qubes.VMShell"
 sudo chmod 0644 "$RPCDIR/qubes.VMShell"
 sudo echo "exec bash" > "$RPCDIR/qubes.VMShell"
 sudo cat "$RPCDIR/qubes.VMShell"
 
 #policy allow for controller/manager
+sudo rm -rf "$RPCDIR/policy/qubes.VMShell"
 sudo touch "$RPCDIR/policy/qubes.VMShell"
 sudo chmod 0664 "$RPCDIR/policy/qubes.VMShell"
 sudo echo "$NAME_BOBMSHELL_CLIENT dom0 allow" > "$RPCDIR/policy/qubes.VMShell"
 sudo echo "$NAME_BOBMSHELL_CLIENT @anyvm allow" >> "$RPCDIR/policy/qubes.VMShell"
+sudo echo "$NAME_BOBMSHELL_CLIENT fedrora34-ProxyWork allow" >> "$RPCDIR/policy/qubes.VMShell"
 sudo cat "$RPCDIR/policy/qubes.VMShell"
 
-#install git and ansible
-sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" 'sudo dnf install git ansible nano -y'
+#install git and ansible in tamplateVM
+sudo qvm-run --pass-io "$BASED_VM" 'sudo dnf install git ansible nano -y'
+sudo qvm-shutdown "$BASED_VM" --wait
+sudo qvm-start "$BASED_VM"
+
+
 
 #set nevm in BOBMSHELL client
 sudo qvm-prefs "$NAME_BOBMSHELL_CLIENT" netvm "$ANSIBLE_NET_VM"
+
+
+#install git and ansible in APPVM
+sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" 'sudo dnf install git ansible nano -y'
 
 
 sudo qvm-shutdown "$BASED_VM" --wait
@@ -46,32 +57,11 @@ sudo qvm-shutdown "$NAME_BOBMSHELL_CLIENT" --wait
 
 sudo qvm-start "$NAME_BOBMSHELL_CLIENT"
 
-#remove dir
-sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "sudo rm -rf $WORK_DIR"
-
 #install ansible-qubes GIT
 sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "git clone https://github.com/ponideal/autoqubes.git"
 
-#transfer confuguration in controller
-#sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "sudo rm -rf $ANSIBLE_ROOT_DIR/"
-
-#transef ansible cfg 
-sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "sudo cp -rf $WORK_DIR/ansible-qubes/ansible.cfg $ANSIBLE_ROOT_DIR/ansible.cfg"
-
-#transef host file
-sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "sudo cp -rf $WORK_DIR/ansible-qubes/hosts $ANSIBLE_ROOT_DIR/hosts"
-
-#ping test to dom0
-sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "ansible dom0 -m ping"
-
-
-#sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "xterm"
-#sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "nautilus"
-
-
-
-
-
+sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "chmod +x $WORK_DIR/controller_install.sh"
+sudo qvm-run --pass-io "$NAME_BOBMSHELL_CLIENT" "sudo ./$WORK_DIR/controller_install.sh"
 
 
 #sudo touch /etc/bash.profile
